78f810dc20aa8e668ca3dc3d84e8653dc85a4d1e
Joshua Moreira <joshuamoreira3@gmail.com>
2024-11-06 11:30:02 -0600

    Updated project to build into single file to fix CORS errors
    This is done using the vite single file plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import { viteSingleFile } from "vite-plugin-singlefile"
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    viteSingleFile()
   ],
   resolve: {
     alias: {
