30fc46b842a9235426daf8b1693711933af8163f
Joshua Moreira <joshuamoreira3@gmail.com>
2024-11-06 14:17:22 -0600

    Removed template files

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import BingoBoard from './components/BingoBoard.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <BingoBoard msg="This is the Terraria Bingo Zone™!" />
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
---
+++ src/components/AppContainer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+    <div class="parent">
+        <div class="div1"> </div>
+    </div>
+</template>
+
+<style scoped>
+.parent {
+    display: grid;
+    grid-template-columns: 0.5fr repeat(3, 1fr) 0.5fr;
+    grid-template-rows: repeat(4, 1fr) 0.5fr;
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+}
+
+.div1 { grid-area: 2 / 2 / 5 / 5; }
+</style>
