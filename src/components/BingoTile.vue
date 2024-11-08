f608d7da4894dca9d2d0a2072d512e0799a032d8
Joshua Moreira <joshuamoreira3@gmail.com>
2024-11-07 22:35:00 -0600

    Updated tile component to be clickable with colour

--- src/components/BingoTile.vue
+++ src/components/BingoTile.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+
 defineProps<{
   msg: string,
   difficulty: number
 }>()
+
+const stateToColourList = ["blank-tile","collected-tile","taken-tile"]
+const tileState = ref(0);
+
+const updateState = () => {
+    tileState.value = (tileState.value+1) % stateToColourList.length;
+}
+
+// Function to reset the `tileState` to "blank-tile" on right-click
+const resetState = (event: Event) => {
+  event.preventDefault(); // Prevent default right-click menu
+  tileState.value = 0;
+};
+
+const tileClass = computed(() => stateToColourList[tileState.value]);
+
 </script>
 
 <template>
-  <div class="tile">
+  <button class="tile" :class="tileClass" @click="updateState" @contextmenu.prevent="resetState">
     <p>{{ msg }}</p>
-  </div>
+  </button>
 </template>
 
 <style scoped>
@@ -18,6 +36,19 @@ defineProps<{
     display: flex;
     justify-content: center;
     align-items: center;
+    cursor: pointer;
+}
+
+.blank-tile {
+    background-color: white;
+}
+
+.collected-tile {
+    background-color: limegreen;
+}
+
+.taken-tile {
+    background-color: red;
 }
 
 </style>
