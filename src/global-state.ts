7438bab56cfa8f79ce95230353dc71f9a7a8b14b
Joshua Moreira <joshuamoreira3@gmail.com>
2024-11-12 15:20:35 -0600

    Update functionality of randomization and fixed spelling mistake

--- src/global-state.ts
+++ src/global-state.ts
@@ -1,28 +1,36 @@
 import { ref } from 'vue';
 import json from "./tile-data.json";
-import { shuffle } from './useful-funcitons';
+import { shuffle } from './useful-functions';
 
 const allBingoTiles = json.tasks;
+const MAX_SEED = 999999999;
 
 // state declared here will be shared
 export const boardSize = ref(4);
-export const currentRandomSeed = ref(Math.random());
+export const currentRandomSeed = ref(Math.floor(Math.random()*MAX_SEED));
 export const bingoTiles = ref(allBingoTiles.slice(0, boardSize.value*boardSize.value));
+
+console.log("The Current Random Seed is:",currentRandomSeed.value);
 
 export function useComposable() {
 
     const setBoardSize = (newSize: number) => { boardSize.value = newSize; }
     const setCurrentRandomSeed = (newSeed: number) => { currentRandomSeed.value = newSeed}
 
-    const randomizeList = () => shuffle(allBingoTiles, currentRandomSeed.value);
+    const randomizeListWithSeed = (newSeed: number) => {
+      setCurrentRandomSeed(newSeed);
+      shuffle(allBingoTiles, currentRandomSeed.value)
+    };
 
-    // Setters and getters for both
-    // can be defined here
+    const randomizeList = () => {
+      randomizeListWithSeed(Math.floor(Math.random()*MAX_SEED));
+    };
 
     return {
         currentRandomSeed,
         boardSize,
         randomizeList,
+        randomizeListWithSeed,
         setBoardSize,
         setCurrentRandomSeed
     };
