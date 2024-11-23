8d7401cb3e79a5e7128da6f29a2778cb63b4160d
Joshua Moreira <51462317+OfficialArms@users.noreply.github.com>
2024-11-23 11:33:02 -0600

    Joshua/update random tile generation (#4)
    Added the ability to see your terraria bingo card's seed and the option
    to set your board to a specific seed. This will now allow players to
    play a game together using the same bingo board.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import BingoBoard from './components/BingoBoard.vue';
 import BingoHeader from './components/BingoHeader.vue';
+import { bingoTiles, boardSize, currentRandomSeed } from './global-state';
+import { randomizeSeed } from './util/randomization';
+
+const newSeed = ref();
+
+const updateSeed = () => {
+  currentRandomSeed.value = newSeed.value;
+};
 </script>
 
 <template>
   <div class="parent-grid">
-      <BingoHeader msg="This is the Terraria Bingo Zone™!"/>
-      <BingoBoard :size="4"/>
+    <BingoHeader msg="Terraria Bingo" />
+    <div style="display: flex; flex-direction: row">
+      <p>Seed: {{ currentRandomSeed }}</p>
+      <button @click="randomizeSeed">🎲</button>
+    </div>
+    <div>
+      <label>Set Seed</label>
+      <input type="number" v-model.lazy="newSeed" :placeholder="'' + currentRandomSeed" />
+      <button @click="updateSeed">Confirm</button>
+    </div>
+    <BingoBoard class="bingo-board-area" :size="boardSize" :bingo-tiles="bingoTiles" />
   </div>
 </template>
 
@@ -15,7 +33,7 @@ import BingoHeader from './components/BingoHeader.vue';
   height: 100%;
   display: flex;
   flex-direction: column;
-  background-image: url("/src/images/background.png");
+  background-image: url('/src/images/background.png');
   background-size: cover;
   color: white;
 }
