02510bb636ef874a1f30bb7c9c78c1c6fd279363
Joshua Moreira <joshuamoreira3@gmail.com>
2024-11-25 21:17:27 -0600

    Updated settings button to use Terraria images

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@ import BingoSettings from './components/BingoSettings.vue';
 import { bingoTiles, boardSize, currentRandomSeed } from './global-state';
 import { randomizeSeed } from './util/randomization';
 import { ALL_BINGO_TILES } from './util/config';
+import SettingsButtonIcon from './images/SettingsButtonicon.png';
+import SettingsButtonIconHighlighted from './images/SettingsButtonIconHighlighted.png';
 
 const isDialogOpen = ref(false);
 
@@ -17,6 +19,19 @@ const toggleSettings = () => {
 watch(boardSize, () => {
   bingoTiles.value = ALL_BINGO_TILES.slice(0, boardSize.value * boardSize.value);
 });
+
+//TODO: MOVE THIS STUFF TO THEIR OWN COMPONENT
+
+const settingsIcons = {
+  onHover: SettingsButtonIconHighlighted,
+  offHover: SettingsButtonIcon
+};
+
+const settingsSource = ref(settingsIcons.offHover);
+
+const setSettingMouseHover = () => { settingsSource.value = settingsIcons.onHover; };
+const setSettingMouseLeave = () => { settingsSource.value = settingsIcons.offHover; }
+
 </script>
 
 <!-- // TODO: make is so that you can close the menu by clicking outside the modal -->
@@ -28,10 +43,11 @@ watch(boardSize, () => {
       <div style="display: flex; flex-direction: row; justify-content: space-between">
         <div style="display: flex; flex-direction: row">
           <p>Seed: {{ currentRandomSeed }}</p>
-          <button @click="randomizeSeed">🎲</button>
+          <img class="random-button" src="./images/RandomDieIcon.png" @click="randomizeSeed" />
         </div>
         <div class="bingo-settings-button">
-          <button :onclick="toggleSettings">Settings</button>
+          <img class="settings-button" v-bind:src="settingsSource" :onclick="toggleSettings"
+            @mouseover="setSettingMouseHover" @mouseleave="setSettingMouseLeave" />
         </div>
       </div>
       <BingoSettings v-if="isDialogOpen" />
@@ -62,5 +78,16 @@ watch(boardSize, () => {
   background-image: url('/src/images/background.png');
   background-size: cover;
   color: white;
+}
+
+.random-button {
+  image-rendering: pixelated;
+  display: block;
+  margin: auto;
+  cursor: pointer;
+}
+
+.settings-button {
+  cursor: pointer;
 }
 </style>
