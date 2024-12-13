d0e7838477096e8e0f83fe1de3c21d29f7afef34
Joshua Moreira <joshuamoreira3@gmail.com>
2024-12-13 02:39:44 -0600

    Moved the tooltip to it's own component

---
+++ src/components/Settings/TooltipDisplay.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  tooltip: string;
+}>();
+
+</script>
+
+<template>
+  <p class="tooltip-text">
+    {{props.tooltip}}
+  </p>
+</template>
+
+<style>
+
+.tooltip-text {
+  text-align: center;
+  background-image: url('../../images/TooltipBackground.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100% 100%;
+  image-rendering: pixelated;
+  margin: 0;
+  padding: 30px 0;
+  height: 100%;
+  /* display: flex;
+  flex-direction: column;
+  position: fixed;
+  align-self: center;
+  z-index: 100;
+  background-color: #18043f;
+  border-radius: 10px;
+  top: 25%;
+  max-width: 60vw;
+  max-height: calc(80vh - 12.5vh - 25vh);
+  min-height: 50vh;
+  min-width: 50vw; */
+}
+</style>
